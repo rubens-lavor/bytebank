cc2ee9318ea1f72dded90f3caf26eba39def4909
rubens-lavor <haryson07@gmail.com>
2022-11-01 21:00:08 -0300

    init project

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Bem vindo ao Bytebank")
+    val titular = "Rubens"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+}
