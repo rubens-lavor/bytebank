7fe00b508d3b5655c8f772e92531f6fbc7455c86
rubens-lavor <haryson07@gmail.com>
2022-11-01 22:40:57 -0300

    for e while

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,60 @@
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular = "Rubens"
-    val numeroConta = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+    var i = 1
+
+    while (i <= 5) {
+        val titular = "Rubens $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+
+        i++
+    }
+
+//    loop@ for (i in 1..100) {
+//        println("i $i")
+//        for (j in 1..100) {
+//            println("j $j")
+//            if (j == 5) break@loop
+//        }
+//    }
+//    for (i in 5 downTo 1 step 1) {
+//
+//        if (i == 4) {
+//            break
+//        }
+//
+//        val titular = "Rubens $i"
+//        val numeroConta = 1000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//
+//    }
+    //testarCondicoes(saldo)
+
+}
+
+fun testarCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
