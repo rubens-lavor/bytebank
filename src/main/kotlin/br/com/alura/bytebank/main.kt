a0a311da25ab5d6639300b3101bd70cfff9852b4
rubens-lavor <haryson07@gmail.com>
2022-11-12 20:38:16 -0300

    tostring, equals and hashcode

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,8 +1,30 @@
-import br.com.alura.bytebank.modelo.Conta
+package br.com.alura.bytebank
+
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    testaContasDiferentes()
+    val endereco = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "casa",
+        cep = "00000-000"
+    )
+    val enderecoNovo = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "casa",
+        cep = "00000-000"
+    )
 
-    println("Total de contas: ${Conta.total}")
+    println(endereco.equals(enderecoNovo))
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+
+    println(endereco.toString())
+    println(enderecoNovo.toString())
+}
+
+fun imprime(valor: Any = 0) {
+    println(valor)
 }
