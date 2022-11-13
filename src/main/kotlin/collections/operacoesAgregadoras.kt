fe52e15010633c2582a997ceb2fb9eacc7ce3502
rubens-lavor <haryson07@gmail.com>
2022-11-13 20:38:56 -0300

    val idades: IntArray = intArrayOf(10, 12, 18, 33, 40, 67)

---
+++ src/main/kotlin/collections/operacoesAgregadoras.kt
@@ -0,0 +1,27 @@
+package collections
+
+fun operacoesAgregadoras(){
+    val idades: IntArray = intArrayOf(10, 12, 18, 33, 40, 67)
+
+    val maiorIdade = idades.max()
+
+    println("Maior idade: $maiorIdade")
+
+    val menorIdade = idades.min()
+    println("Menor idade: $menorIdade")
+
+    val media = idades.average()
+    println("media: $media")
+
+    val todosMaioresDeIdade = idades.all { it >= 18 }
+    println("Todos maiores? $todosMaioresDeIdade")
+
+    val existeMaiorDeIdade = idades.any { it >= 18 }
+    println("Algum aluno pe aior de idade? $existeMaiorDeIdade")
+
+    val maioresDeIdade: List<Int> = idades.filter { it >= 18 }
+    println("maiores de 18 anos: $maioresDeIdade")
+
+    val busca = idades.find { it > 18 }
+    println("Busca: $busca")
+}
