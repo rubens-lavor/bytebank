1d0cebb20aa35aa7496adeff123c1144226831b3
rubens-lavor <haryson07@gmail.com>
2022-11-13 12:56:53 -0300

    classe SaldoInsuficienteException

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,8 +1,6 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.teste.testaExpressao
-import java.lang.ClassCastException
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 
 fun main() {
     println("início main")
@@ -14,12 +12,9 @@ fun funcao1(){
     println("início funcao1")
     try {
         funcao2()
-    } catch (e: ClassCastException) {
-        println(e.message)
-        println(e.stackTrace)
-        println(e.cause)
+    } catch (e: SaldoInsuficienteException) {
         e.printStackTrace()
-        println("ClassCastException foi capturada")
+        println("SaldoInsuficienteException foi capturada")
     }
     println("fim funcao1")
 }
@@ -28,8 +23,7 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5){
         println(i)
-        val endereco = Any()
-        throw ClassCastException()
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
