adf31165ee051e9e70251f30df16e955dd950947
rubens-lavor <haryson07@gmail.com>
2022-11-13 20:24:53 -0300

    exemplo ranges

--- src/main/kotlin/collections/main.kt
+++ src/main/kotlin/collections/main.kt
@@ -1,23 +1,5 @@
 package collections
 
 fun main() {
-    val idades: IntArray = intArrayOf(25,19,33,20,55,40)
 
-    var maiorIdade = idades.get(0)
-    for (idade: Int in idades) {
-        if (idade > maiorIdade) {
-            maiorIdade = idade
-        }
-    }
-
-    println("maior idade: $maiorIdade")
-
-    var menorIdade = idades.get(0)
-    idades.forEach { idade ->
-        if (idade < menorIdade) {
-            menorIdade = idade
-        }
-    }
-
-    println("menor idade: $menorIdade")
 }
---
+++ src/main/kotlin/collections/range.kt
@@ -0,0 +1,30 @@
+package collections
+
+fun exemploRanges(){
+    val serie: IntRange = 1.rangeTo(10)
+
+    for (s: Int in serie) {
+        print("$s ")
+    }
+    println()
+
+    val numerosPares: IntProgression = 0.until(10) step 2
+    val numerosParesReverso: IntProgression = 10 downTo (0) step 2
+
+    val intervalo: ClosedFloatingPointRange<Double> = 1500.0..5000.0
+    val salario = 14000.0
+    if (salario in intervalo) {
+        println("Está dentro do intervalo")
+    } else {
+        println("fora do intervalo")
+    }
+
+    val alfabeto: CharRange = 'a'..'z'
+    val letra = 'k'
+    println(letra in alfabeto)
+
+
+    println(serie.toList())
+    println(numerosPares.toList())
+    println(numerosParesReverso.toList())
+}
