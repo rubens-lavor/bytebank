77cf312ba6d0f76646b999825dd5e14d3de5d824
rubens-lavor <haryson07@gmail.com>
2022-11-14 23:19:25 -0300

    listas imutáveis

---
+++ src/main/kotlin/collections/list/Prateleira.kt
@@ -0,0 +1,14 @@
+package collections.list
+
+class Prateleira (
+    val genero: String,
+    val livros: List<Livro>
+) {
+    fun organizarPorAutor(): List<Livro> {
+        return livros.sortedBy { it.autor }
+    }
+
+    fun organizarPorAnoPublicacao(): List<Livro> {
+        return livros.sortedBy { it.anoPublicacao }
+    }
+}
---
+++ src/main/kotlin/collections/list/usaPrateleira.kt
@@ -0,0 +1,11 @@
+package collections.list
+
+fun main() {
+    val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
+
+    val porAutor = prateleira.organizarPorAutor()
+    val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
+
+    porAutor.imprimeComMarcadores()
+    porAnoPublicacao.imprimeComMarcadores()
+}
