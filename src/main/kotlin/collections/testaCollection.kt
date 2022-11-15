3de481ecaed9b26651088b33fa6b11ba7266379a
rubens-lavor <haryson07@gmail.com>
2022-11-15 12:07:25 -0300

    testa collection

---
+++ src/main/kotlin/collections/testaCollection.kt
@@ -0,0 +1,21 @@
+package collections
+
+fun testaCopia() {
+    val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
+    banco.salva("rubens")
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        Companion.dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
